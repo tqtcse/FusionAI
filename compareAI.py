9b5b86d21db011d567bf82d9758a070b0e01d59d
Toan <maristran72@gmail.com>
2025-01-04 21:58:00 +0700

    update UI and compareModel

--- compareAI.py
+++ compareAI.py
@@ -1,5 +1,8 @@
 from gemini import Gemini_Model
 from llama import Llama_Model
+from gpt import GPT_Model
+
+
 
 class Compare_Model:
 	def GeminiVsLlama(user_input):
@@ -10,8 +13,35 @@ class Compare_Model:
 
 		output = "Gemini: " + geminiOutput + "Llama: " + llamaOutput
 		return output
+
+	def GeminiVsGPT(user_input):
+		print("Gemini Model: ")
+		geminiOutput = Gemini_Model.output(user_input)
+		print("GPT Model: ")
+		gptOutput = GPT_Model.output(user_input)
+		output = "Gemini: " + geminiOutput + "GPT: " + gptOutput
+		return output
+
+	def GPTVsLlama(user_input):
+		print("GPT_Model: ")
+		gptOutput = GPT_Model.output(user_input)
+		print("Llama Model: ")
+		llamaOutput = Llama_Model.output(user_input)
+		output = "Llama Model: " + llamaOutput + "GPT: " + gptOutput
+		return output
+
+	def compareAll(user_input):
+		print("Gemini Model: ")
+		geminiOutput = Gemini_Model.output(user_input)
+		print("Llama Model: ")
+		llamaOutput = Llama_Model.output(user_input)
+		print("GPT Model: ")
+		gptOutput = GPT_Model.output(user_input)
+		
+		output = "Gemini: " + geminiOutput + "Llama: " + llamaOutput + "GPT: " + gptOutput
+		return output
 		
 	def analyzeOutput(user_input):
 		#geminiAnalyze
 		geminiOutput = Gemini_Model.output("analyze these answer of each model " + user_input)
-		geminiOutput2 = Gemini_Model.output("Please only list the answers chosen by each model (without any explanations) in the format Model Name: A,B,C,D. Analyze the answers of each model." + user_input)
+		geminiOutput2 = Gemini_Model.output("Only list the answers chosen by each model (without any explanations) in the format Model Name: A,B,C,D. Analyze the answers of each model." + user_input)
--- main.py
+++ main.py
@@ -6,13 +6,15 @@ from compareAI import Compare_Model
 
 
 if __name__ == "__main__":
-	print("-------------------------   Welcome to FusionAI  ---------------------------\nSelect Model:")
-	for model in ["GPT", "Gemini", "Llama", "Claude"]:
-		print(f"- {model}")
-	print("CompareAI: ")
-	print("1. Gemini vs Llama")
-
 	while True:
+		print("-------------------------   Welcome to FusionAI  ---------------------------\nSelect Model:")
+		for model in ["GPT", "Gemini", "Llama", "Claude"]:
+			print(f"- {model}")
+		print("CompareAI: ")
+		print("1. Two Model")
+		print("2. All Model")
+
+	
 		command_line = input("\n>")
 
 	#Model
@@ -61,17 +63,37 @@ if __name__ == "__main__":
 		#Compare
 
 		elif command_line == "1":
-			print("Comparing Gemini and Llama")
+			print("Comparing Two Model Mode: ")
+			print("1. GPT vs Gemini:\n2. GPT vs LLama:\n3. Gemini vs Llama")
 			while True:
 				user_input = input("CompareModel> ")
 
 				if user_input == "exit":
-					print("Exiting Model.")
+					print("Exiting Compare Mode.")
 					break
-				if user_input != "A":
-					compare_Output = Compare_Model.GeminiVsLlama(user_input)
+
+				if user_input == "1":
+					print("GPT vs Gemini: ")
+					user_input = input("GPTvsGemini> ")
+					compare_Output = Compare_Model.GeminiVsGPT(user_input)
 					print("If you want to analyze these output, type A")
+
+				if user_input == "2":
+					print("GPT vs Llama: ")
+					user_input = input("GPTvsLlama> ")
+					compare_Output = Compare_Model.GPTvsLlama(user_input)
+					print("If you want to analyze these output, type A")
+
+				if user_input == "3":
+					print("Gemini vs LLama: ")
+					while True:
+						user_input = input("GeminiVsLlama> ")
+						if user_input == "exit":
+							break
+						compare_Output = Compare_Model.GeminiVsLlama(user_input)
+						print("If you want to analyze these output, type A")
 				
+
 				if user_input == "A":
 					Compare_Model.analyzeOutput(compare_Output)
 					while True:
@@ -80,6 +102,15 @@ if __name__ == "__main__":
 							print("Exiting Analyze Mode...")
 							break
 
+
+		elif command_line == "2":
+			print("Comparing All Models Mode: ")
+			while True:
+				user_input = input("CompareAllModels> ")
+				if user_input == "exit":
+					break
+				compare_Output = Compare_Model.compareAll(user_input)
+				print("If you want to analyze these output, type A")
 
 
 		elif command_line == "exit":
